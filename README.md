060270e90d0999ed007ccc253abc7400f1a9d646
Mikael Kundert <mikael.kundert@wunderkraut.com>
2016-11-22 12:29:18 +0200

    Initial commit, does nothing yet (just project specs)

---
+++ README.md
@@ -0,0 +1,3 @@
+# jQuery Favorite Widget
+Provides an widget for adding or removing selected content from favorites that
+is stored in cookies.
---
+++ jquery.favoritewidget.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2016, Mikael Kundert
+ * Dual licensed under the MIT or GPL Version 3 licenses.
+ *   - http://jquery.org/license
+ *   - https://www.gnu.org/licenses/gpl-3.0.txt
+ * 
+ */
+
+(function($) {
+  $.widget( "ui.favoritewidget", {
+    // @TODO
+  });
+})(jQuery);
