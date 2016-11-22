7bc6ab6faa8eb5841271317982439a8ffa63b83d
Mikael Kundert <mikael.kundert@wunderkraut.com>
2016-11-22 12:40:04 +0200

    Added local LICENSE file reference

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # jQuery Favorite Widget
 Provides an widget for adding or removing selected content from favorites that
 is stored in cookies.
+
+# License
+This project is dual licensed with [MIT or GPL v3](LICENSE).
--- jquery.favoritewidget.js
+++ jquery.favoritewidget.js
@@ -1,9 +1,6 @@
 /*
  * Copyright 2016, Mikael Kundert
  * Dual licensed under the MIT or GPL Version 3 licenses.
- *   - http://jquery.org/license
- *   - https://www.gnu.org/licenses/gpl-3.0.txt
- * 
  */
 
 (function($) {
